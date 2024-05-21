c391042a2a171281af46b0ec7c4cb3512e454042
stefano <baiardi10@gmail.com>
2024-05-21 17:22:39 +0200

    add field pnnr

--- lib/ui/pages/pois_section/detail_poi_page.dart
+++ lib/ui/pages/pois_section/detail_poi_page.dart
@@ -5,8 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 import 'package:municipium/bloc/point_of_interest_list_bloc/point_of_interest_list_bloc.dart';
 import 'package:municipium/services/network/dto/poi_detail_dto.dart';
+import 'package:municipium/utils/icons_utils.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 @RoutePage()
 class DetailPoiPage extends StatelessWidget implements AutoRouteWrapper {
@@ -43,7 +47,36 @@ class DetailPoiPage extends StatelessWidget implements AutoRouteWrapper {
                     ),
                     _buildRowArgument(state.poiDetailDTO),
                     _buildRowCategories(state.poiDetailDTO),
-                    _buildImages(state.poiDetailDTO)
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    _buildOpenDataLogo(state.poiDetailDTO),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    _buildImages(state.poiDetailDTO),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    _buildDescription(state.poiDetailDTO),
+                    const Divider(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    _buildRowElement(
+                      Icons.place,
+                      state.poiDetailDTO.address,
+                      () => MapsLauncher.launchCoordinates(
+                          double.parse(state.poiDetailDTO.latitude!),
+                          double.parse(state.poiDetailDTO.longitude!)),
+                    ),
+                    _buildRowElement(
+                        Icons.place, state.poiDetailDTO.openingTimes, null),
+                    _buildRowElementLink(
+                        CupertinoIcons.link, state.poiDetailDTO.webSite),
+                    _buildRowElement(Icons.accessible,
+                        state.poiDetailDTO.modalitaAccesso, null),
+                    _buildContactPoints(state.poiDetailDTO.puntiContatto)
                   ],
                 ),
               );
@@ -65,21 +98,172 @@ class DetailPoiPage extends StatelessWidget implements AutoRouteWrapper {
     );
   }
 
+  Widget _buildContactPoints(List<PuntiContatto>? puntiDiContatto) {
+    if (puntiDiContatto != null && puntiDiContatto.isNotEmpty) {
+      return ListView.builder(
+        shrinkWrap: true,
+        primary: false,
+        itemCount: puntiDiContatto.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 40,
+            padding: EdgeInsets.symmetric(vertical: 6),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  fit: FlexFit
+                      .tight, // Usa 'tight' per consumare tutto lo spazio disponibile
+                  child: Row(
+                    children: [
+                      Icon(PnnrUtils.getIconsFromContactPointPnnr(
+                          puntiDiContatto[index].tipo!)),
+                      const SizedBox(width: 10),
+                      Text(puntiDiContatto[index].label!)
+                    ],
+                  ),
+                ),
+                // Usa 'loose' per permettere al widget di occupare solo lo spazio necessario
+                Container(
+                  width: 200,
+                  child: InkWell(
+                    child: Text(
+                      puntiDiContatto[index].valore!,
+                      textAlign: TextAlign.end,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    onTap: () {
+                      // Assicurati di invocare il metodo correttamente con il parametro
+                      final url = PnnrUtils.getUrlFromContactPointPnnr(
+                          puntiDiContatto[index].tipo!,
+                          puntiDiContatto[index].valore!);
+                      if (url != null) {
+                        launchUrl(url);
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _buildRowElement(
+      IconData iconData, String? text, void Function()? onTap) {
+    if (text != null) {
+      if (text.isNotEmpty) {
+        return InkWell(
+          onTap: onTap,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(iconData),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: Text(text)),
+                ],
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+            ],
+          ),
+        );
+      } else {
+        return Container();
+      }
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _buildRowElementLink(IconData iconData, String? text) {
+    if (text != null) {
+      if (text.isNotEmpty) {
+        return Column(
+          children: [
+            SizedBox(
+              height: 50,
+              child: Row(
+                children: [
+                  Icon(iconData),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(
+                    child: InkWell(
+                      child: Text(text),
+                      onTap: () => launchUrl(Uri.parse(text)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+          ],
+        );
+      } else {
+        return Container();
+      }
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _buildOpenDataLogo(PoiDetailDTO poiDetailDTO) {
+    if (poiDetailDTO.openDataLogo != null) {
+      return Image.network(
+        '${poiDetailDTO.openDataLogo!.baseUrl}${poiDetailDTO.openDataLogo!.i320}',
+        width: 200,
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _buildDescription(PoiDetailDTO poiDetailDTO) {
+    if (poiDetailDTO.description != null) {
+      return Html(data: poiDetailDTO.description);
+    } else {
+      return Container();
+    }
+  }
+
   Widget _buildImages(PoiDetailDTO poiDetailDTO) {
     if (poiDetailDTO.images != null) {
-      if (poiDetailDTO.images!.isNotEmpty) {
+      if (poiDetailDTO.images!.length > 1) {
         return CarouselSlider(
-          options: CarouselOptions(),
+          options: CarouselOptions(viewportFraction: 1),
           items: poiDetailDTO.images!
               .map((item) => Container(
                     child: Center(
-                        child: Image.network(item.i640!,
-                            fit: BoxFit.cover, width: 1000)),
+                        child: Image.network('${item.baseUrl}${item.i640!}',
+                            fit: BoxFit.contain, height: 440, width: 1000)),
                   ))
               .toList(),
         );
       } else {
-        return Container();
+        if (poiDetailDTO.image != null) {
+          return Container(
+            child: Center(
+                child: Image.network(
+                    '${poiDetailDTO.image!.baseUrl}${poiDetailDTO.image!.i640}',
+                    fit: BoxFit.contain,
+                    height: 400,
+                    width: 1000)),
+          );
+        } else {
+          return Container();
+        }
       }
     } else {
       return Container();
--- lib/ui/pages/pois_section/maps_page.dart
+++ lib/ui/pages/pois_section/maps_page.dart
@@ -22,11 +22,10 @@ import 'package:municipium/ui/components/maps_component/filter_modal_bottomsheet
 import 'package:municipium/ui/components/menu/menu_drawer.dart';
 import 'package:municipium/ui/components/point_of_interests/custom_info_window.dart';
 
-
 @RoutePage()
 class MapsPage extends StatefulWidget implements AutoRouteWrapper {
   const MapsPage({super.key, required this.scaffoldKey});
-  final GlobalKey<ScaffoldState> scaffoldKey; 
+  final GlobalKey<ScaffoldState> scaffoldKey;
   @override
   State<MapsPage> createState() => _MapsPageState();
 
@@ -38,17 +37,17 @@ class MapsPage extends StatefulWidget implements AutoRouteWrapper {
                 ..fetchPointOfInterestList(),
         ),
         BlocProvider<CategoryPoiBloc>(
-      create: (context) =>
-          CategoryPoiBloc(pointOfInterestRepository: context.read())
-            ..fetchCategoryPoiList(),
+          create: (context) =>
+              CategoryPoiBloc(pointOfInterestRepository: context.read())
+                ..fetchCategoryPoiList(),
         ),
         BlocProvider<CategorySelectionCubit>(
-        create: (_) => CategorySelectionCubit(),)
+          create: (_) => CategorySelectionCubit(),
+        )
       ], child: this);
 }
 
 class _MapsPageState extends State<MapsPage> {
-  
   final CustomInfoWindowController _customInfoWindowController =
       CustomInfoWindowController();
   final Completer<GoogleMapController> _controller =
@@ -87,182 +86,185 @@ class _MapsPageState extends State<MapsPage> {
             as StoredMunicipalityGlobalState)
         .municipality;
     return BlocConsumer<PointOfInterestBloc, PointOfInterestState>(
-      listener: (context, state) {
-        if(state is FetchingPointOfInterestListState) {
-          SVProgressHUD.show(status: AppLocalizations.of(context)!
-                                          .loading_sv_message_poi,);
-        }else {
-          SVProgressHUD.dismiss();
-        }
-      },
-      builder: (context, state) {
-        Set<Marker> markers = {};
-        if (state is FetchedPointOfInterestListState) {
-          if (state.pointOfInterestsList.pointOfInterestsItemList != null) {
-            markers = state.pointOfInterestsList.pointOfInterestsItemList!
-                .map((point) => Marker(
-                    icon: point.pointOfInterestCategories!.isNotEmpty
-                        ? BitmapDescriptor.defaultMarkerWithHue(
-                            _convertColorToHue(
-                                point.pointOfInterestCategories?.first))
-                        : BitmapDescriptor.defaultMarker,
-                    markerId: MarkerId(point.id.toString()),
-                    position: LatLng(double.parse(point.latitude!),
-                        double.parse(point.longitude!)),
-                    onTap: () {
-                      _customInfoWindowController.addInfoWindow!(
-                          Column(
-                            children: [
-                              Container(
-                                color: Theme.of(context).primaryColor,
-                                padding: const EdgeInsets.all(16),
-                                width: width,
-                                
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      height:30,
-                                      child: Text(
-                                        point.name!,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 10,
-                                    ),
-                                    Text(point.address!),
-                                    const SizedBox(
-                                      height: 30,
-                                    ),
-                                    FullWidthConfirmButton(
-                                      isEnabled: true,
-                                      onTap: () => context.pushRoute(DetailPoiRoute(poiId: point.id!)),
-                                      height: 40,
-                                      text: AppLocalizations.of(context)!
-                                          .btn_go_open_detail,
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Triangle.isosceles(
-                                edge: Edge.BOTTOM,
-                                child: Container(
-                                  color: Theme.of(context).primaryColor,
-                                  width: 20.0,
-                                  height: 20.0,
-                                ),
-                              ),
-                            ],
-                          ),
-                          LatLng(double.parse(point.latitude!),
-                              double.parse(point.longitude!)),
-                          offset,
-                          230,
-                          height);
-                    }))
-                .toSet();
-          }
-        }
-        return Stack(
-            children: [
-              Positioned.fill(
-                child: GoogleMap(
-                  
-                  onTap: (position) {
-                    _customInfoWindowController.hideInfoWindow!();
-                  },
-                  onCameraMove: (position) {
-                    _customInfoWindowController.onCameraMove!();
-                  },
-                  myLocationButtonEnabled: false,
-                  markers: markers,
-                  initialCameraPosition: CameraPosition(
-                      zoom: 14,
-                      target: LatLng(municipality.lat, municipality.lng)),
-                  onMapCreated: (GoogleMapController controller) {
-                    _customInfoWindowController.googleMapController = controller;
-          
-                    _controller.complete(controller);
-                  },
-                ),
-              ),
-              Positioned(
-                top: 50,
-                left: 0,
-                right: 0,
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () => showModalBottomSheet(
-                          isScrollControlled: true,
-            context: context,
-            builder: ((modalContext) => CustomBaseBottomSheet(
-              
-                height: MediaQuery.of(context).size.height * 0.9,
-                title: 'filtri',
-                body: FilterModalBottomSheet(categorySelectionCubit: context.read(), pointOfInterestListBloc: context.read(),)))),
-                        child: Container(
-                            padding: const EdgeInsets.all(14),
-                            decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                borderRadius: BorderRadius.circular(8),
-                                border: Border.all(color: Colors.blueGrey)),
-                            child: const Center(child: FaIcon(FontAwesomeIcons.sliders, size: 18,))),
-                      ),
-                      const SizedBox(width: 6),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
+        listener: (context, state) {
+      if (state is FetchingPointOfInterestListState) {
+        SVProgressHUD.show(
+          status: AppLocalizations.of(context)!.loading_sv_message_poi,
+        );
+      } else {
+        SVProgressHUD.dismiss();
+      }
+    }, builder: (context, state) {
+      Set<Marker> markers = {};
+      if (state is FetchedPointOfInterestListState) {
+        if (state.pointOfInterestsList.pointOfInterestsItemList != null) {
+          markers = state.pointOfInterestsList.pointOfInterestsItemList!
+              .map((point) => Marker(
+                  icon: point.pointOfInterestCategories!.isNotEmpty
+                      ? BitmapDescriptor.defaultMarkerWithHue(
+                          _convertColorToHue(
+                              point.pointOfInterestCategories?.first))
+                      : BitmapDescriptor.defaultMarker,
+                  markerId: MarkerId(point.id.toString()),
+                  position: LatLng(double.parse(point.latitude!),
+                      double.parse(point.longitude!)),
+                  onTap: () {
+                    _customInfoWindowController.addInfoWindow!(
+                        Column(
+                          children: [
+                            Container(
                               color: Theme.of(context).primaryColor,
-                              borderRadius: BorderRadius.circular(8),
-                              border: Border.all(color: Colors.blueGrey)),
-                          child: const TextField(
-                            decoration: InputDecoration(
-                              hintStyle: TextStyle(fontSize: 16),
-                              hintText: 'Cerca',
-                              prefixIcon: Icon(Icons.search),
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.only(
-                                  top: 8, bottom: 0, left: 10, right: 10),
+                              padding: const EdgeInsets.all(16),
+                              width: width,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    height: 30,
+                                    child: Text(
+                                      point.name!,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(point.address!),
+                                  const SizedBox(
+                                    height: 30,
+                                  ),
+                                  FullWidthConfirmButton(
+                                    isEnabled: true,
+                                    onTap: () => context.pushRoute(
+                                        DetailPoiRoute(poiId: point.id!)),
+                                    height: 40,
+                                    text: AppLocalizations.of(context)!
+                                        .btn_go_open_detail,
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
+                            Triangle.isosceles(
+                              edge: Edge.BOTTOM,
+                              child: Container(
+                                color: Theme.of(context).primaryColor,
+                                width: 20.0,
+                                height: 20.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                        LatLng(double.parse(point.latitude!),
+                            double.parse(point.longitude!)),
+                        offset,
+                        230,
+                        height);
+                  }))
+              .toSet();
+        }
+      }
+      return Stack(
+        children: [
+          Positioned.fill(
+            child: GoogleMap(
+              myLocationEnabled: false,
+              onTap: (position) {
+                _customInfoWindowController.hideInfoWindow!();
+              },
+              onCameraMove: (position) {
+                _customInfoWindowController.onCameraMove!();
+              },
+              zoomControlsEnabled: false,
+              myLocationButtonEnabled: false,
+              markers: markers,
+              initialCameraPosition: CameraPosition(
+                  zoom: 14, target: LatLng(municipality.lat, municipality.lng)),
+              onMapCreated: (GoogleMapController controller) {
+                _customInfoWindowController.googleMapController = controller;
+
+                _controller.complete(controller);
+              },
+            ),
+          ),
+          Positioned(
+            top: 50,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => showModalBottomSheet(
+                        isScrollControlled: true,
+                        context: context,
+                        builder: ((modalContext) => CustomBaseBottomSheet(
+                            height: MediaQuery.of(context).size.height * 0.9,
+                            title: 'filtri',
+                            body: FilterModalBottomSheet(
+                              categorySelectionCubit: context.read(),
+                              pointOfInterestListBloc: context.read(),
+                            )))),
+                    child: Container(
+                        padding: const EdgeInsets.all(14),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Colors.blueGrey)),
+                        child: const Center(
+                            child: FaIcon(
+                          FontAwesomeIcons.sliders,
+                          size: 18,
+                        ))),
+                  ),
+                  const SizedBox(width: 6),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(8),
+                          border: Border.all(color: Colors.blueGrey)),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintStyle: TextStyle(fontSize: 16),
+                          hintText: 'Cerca',
+                          prefixIcon: Icon(Icons.search),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                              top: 8, bottom: 0, left: 10, right: 10),
                         ),
                       ),
-                      const SizedBox(
-                        width: 6,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          widget.scaffoldKey.currentState?.openDrawer();
-                        },
-                        child: Container(
-                            padding: const EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                borderRadius: BorderRadius.circular(8),
-                                border: Border.all(color: Colors.blueGrey)),
-                            child: const Center(child: Icon(Icons.menu))),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      widget.scaffoldKey.currentState?.openDrawer();
+                    },
+                    child: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Colors.blueGrey)),
+                        child: const Center(child: Icon(Icons.menu))),
+                  ),
+                ],
               ),
-              CustomInfoWindow(
-                (top, left, width, height) => null,
-                controller: _customInfoWindowController,
-              ),
-            ],
-          );
-      }
-        
-      
-    );
+            ),
+          ),
+          CustomInfoWindow(
+            (top, left, width, height) => null,
+            controller: _customInfoWindowController,
+          ),
+        ],
+      );
+    });
   }
 }
